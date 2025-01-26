a666bec251495d5ae25232268f038166b4f1ceda
Daria <dmescherina@gmail.com>
2025-01-26 20:11:46 +0000

    Update config.py

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,4 +1,4 @@
-DATA_FILE_COEF = 'data/df_all_mean.csv'
-DATA_FILE_SHAP_MEAN = 'data/shap_lgb_mean.csv'
-DATA_FILE_SHAP_SUM = 'data/shap_lgb_sum.csv'
-DATA_FILE_MODEL_PERF = 'data/model_performance.csv'
+DATA_FILE_COEF = 'streamlit_app/data/df_all_mean.csv'
+DATA_FILE_SHAP_MEAN = 'streamlit_app/data/shap_lgb_mean.csv'
+DATA_FILE_SHAP_SUM = 'streamlit_app/data/shap_lgb_sum.csv'
+DATA_FILE_MODEL_PERF = 'streamlit_app/data/model_performance.csv'
